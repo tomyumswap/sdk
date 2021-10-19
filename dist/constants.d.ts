6b744c8ece0fbee111d0fad8e17adbe9451c5a5c
Nikhil Sahare <nikhil.sahare@seracle.com>
2021-10-19 13:28:31 +0530

    initial commit

---
+++ dist/constants.d.ts
@@ -0,0 +1,35 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | bigint | string;
+export declare enum ChainId {
+    MAINNET = 56,
+    TESTNET = 97
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "0xe95AA116E6D39Ac22761A4454e076086EFb1E8A7";
+export declare const INIT_CODE_HASH = "0x08d94357a9ac0859d260e396769ac97540ed7ca31885201779a56039c1f8dcd6";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const FEES_NUMERATOR: JSBI;
+export declare const FEES_DENOMINATOR: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
